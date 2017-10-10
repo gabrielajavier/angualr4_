e7b175fb87fd4fb6e6ac0e9db23c17ca2ef366d5
gcjavier <carolinajavierenriquez@gmail.com>
2017-10-10 14:56:55 -0500

    Reto Crear una vista para editar records primera parte sale error

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { FormsModule } from '@angular/forms';
 import { AgmCoreModule } from '@agm/core';
 import { ResaltarDirective } from './directives/resaltar.directive';
 import { ContarClicksDirective } from './directives/contar-clicks.directive';
@@ -11,19 +11,18 @@ import { Routes, RouterModule } from '@angular/router';
 import { DetalleComponent } from './detalle/detalle.component';
 import { LugaresComponent } from './lugares/lugares.component';
 import { ContactoComponent } from './contacto/contacto.component';
-import { CrearComponent } from './crear/crear.component';
-
 import { LugaresService } from './services/lugares.service';
 import { AngularFireModule } from 'angularfire2';
 import { AngularFireDatabaseModule } from 'angularfire2/database';
 import { AngularFireAuthModule } from 'angularfire2/auth';
+import { CrearComponent } from './crear/crear.component';
 import { HttpModule} from '@angular/http';
 const appRoutes: Routes = [
  {path: '', component: LugaresComponent},
  {path: 'lugares', component: LugaresComponent},
  {path: 'detalle/:id', component: DetalleComponent},
  {path: 'contacto', component: ContactoComponent},
- {path: 'crear', component: CrearComponent},
+ {path: 'crear/id', component: CrearComponent},
 ];
 export const firebaseConfig = {
   apiKey: "AIzaSyAVafGYw6H2cVUmSZUSseuajqBMfnm2Qrw",
--- src/app/crear/crear.component.ts
+++ src/app/crear/crear.component.ts
@@ -1,16 +1,23 @@
 import { Component } from '@angular/core';
 import { LugaresService } from '../services/lugares.service';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-crear',
   templateUrl: './crear.component.html',
 })
-
-
 export class CrearComponent {
 	lugar:any = {};
-	constructor(private lugaresService: LugaresService){
-
+	id: any = null;
+	constructor(private lugaresService: LugaresService, private route: ActivatedRoute){
+		this.id = this.route.snapshot.params['id'];
+		if(this.id != 'new'){
+			this.lugaresService.getLugar(this.id)
+			.valueChanges().subscribe(lugar =>{
+	        console.log(lugar);
+	        this.lugar = lugar;
+    	});
+		}
 	}
 	guardarLugar(){
 		var direccion = this.lugar.calle+','+this.lugar.ciudad+','+this.lugar.pais;
@@ -18,9 +25,14 @@ export class CrearComponent {
 			.subscribe((result) =>{
 				this.lugar.lat = result.json().results[0].geometry.location.lat;
 				this.lugar.lng = result.json().results[0].geometry.location.lng;
-				this.lugar.id = Date.now();
-				this.lugaresService.guardarLugar(this.lugar);
-				alert('Negocio guardado con éxito!');
+				if(this.id != 'new'){
+		            this.lugaresService.editarLugar(this.lugar);
+		            alert('Negocio editado con éxito!');
+		        }else{
+		            this.lugar.id = Date.now();
+		            this.lugaresService.guardarLugar(this.lugar);
+		            alert('Negocio guardado con éxito!');
+		        }
 				this.lugar = {};
 			});
 	}
