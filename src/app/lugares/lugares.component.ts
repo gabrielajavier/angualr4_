04aabc426e041c02c6d34e874ed811bfa1586e2f
gcjavier <carolinajavierenriquez@gmail.com>
2017-10-12 13:52:20 -0500

    Manejando errores HTTP

--- src/app/lugares/lugares.component.ts
+++ src/app/lugares/lugares.component.ts
@@ -23,6 +23,9 @@ export class LugaresComponent {
         var me = this;
         me.lugares = Object.keys(me.lugares).map(function (key) { return me.lugares[key]; });
         debugger;
+      }, error => {
+        console.log (error);
+        alert('Tenemos algo de dificultades, disculpe las molestias. Error: ' + error.statusText)
       });
   }
 }
