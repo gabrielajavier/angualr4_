19f985bf322d2beec99954e010a44f30cf5019f6
gcjavier <carolinajavierenriquez@gmail.com>
2017-10-17 14:23:52 -0500

    transiciones

--- src/app/lugares/lugares.component.ts
+++ src/app/lugares/lugares.component.ts
@@ -1,23 +1,25 @@
 import { Component } from '@angular/core';
 import {LugaresService} from "../services/lugares.service";
-import {trigger, state, style} from "@angular/animations"
+import {trigger, state, style, transition, animate} from "@angular/animations"
 
 @Component({
   selector: 'app-lugares',
   templateUrl: './lugares.component.html',
   styleUrls: ['./lugares.component.css'],
   animations: [
-    trigger('contenedorAnimable',[
-        state('initial', style({
+    trigger('contenedorAnimable', [
+        state('inicial', style({
            opacity:0,
            backgroundColor: 'green',
            transform: 'rotate3d(0,0,0,0deg)'
         })),
-        state('ifinal', style({
+        state('final', style({
            opacity:1,
            backgroundColor: 'yellow',
            transform: 'rotate3d(5,10,20,30deg)'
         })),
+        transition('inicial => final', animate(1000)),
+        transition('final => inicial', animate(500)),
       ])
   ]
 
@@ -26,11 +28,14 @@ import {trigger, state, style} from "@angular/animations"
 /* PROBANDO ROUTER */
 export class LugaresComponent {
   title = 'PlatziSquare';
-  state = 'initial';
+  state = 'final';
 
   lat:number= -12.0482677;
   lng:number= -77.0453564;
   lugares = null;
+  animar(){
+    this.state = (this.state === 'final') ? 'inicial' : 'final';
+  }
   constructor(private lugaresService: LugaresService){
     lugaresService.getLugares()
       /*.valueChanges()*/.subscribe(lugares =>{
